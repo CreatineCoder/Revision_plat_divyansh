36bd2aa7995378cc442ea10fca128da4e71c1dbf
CreatineCoder <devanshagrawal1027@gmail.come>
2025-12-30 18:49:54 +0530

    CORS ignored

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+  const cors = require('cors');
+  app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], allowedHeaders: ['Content-Type', 'Authorization'] }));
 
 // Request logging middleware
 app.use((req, res, next) => {
